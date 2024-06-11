aa3d9d1e74121e4a5f61aa115daff42c85061d0e
JoseLr422 <raperoelement@gmail.com>
2024-06-10 19:56:53 -0500

    Create scripts.JS

---
+++ scripts.JS
@@ -0,0 +1,8 @@
+// scripts.js
+
+// Añadir funcionalidad de clic al botón "Join NOW"
+document.querySelector('.join-now').addEventListener('click', function() {
+    alert('¡Gracias por unirte a MindWell!');
+});
+
+// Puedes añadir más lógica para otras interacciones en la página aquí
