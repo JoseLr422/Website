3128cb9041d1d4767279634d2c87cda95115e8d5
JoseLr422 <raperoelement@gmail.com>
2024-06-27 21:09:38 -0500

    Update scripts.js

--- JS/scripts.js
+++ JS/scripts.js
@@ -6,3 +6,27 @@ document.querySelector('.join-now').addEventListener('click', function() {
 });
 
 // Puedes añadir más lógica para otras interacciones en la página aquí
+
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.querySelector('form[name="frm"]');
+    form.addEventListener('submit', (event) => {
+        const fname = form.elements['fname'].value;
+        const flastname = form.elements['flastname'].value;
+        const femail = form.elements['femail'].value;
+        const fphone = form.elements['fphone'].value;
+        const fcourse = form.elements['fcourse'].value;
+
+        if (!fname || !flastname || !femail || !fphone) {
+            event.preventDefault();
+            alert('Por favor, complete los datos del formulario');
+        } else if (!ValidateEmail(femail)) {
+            event.preventDefault();
+            alert('Por favor, ingrese un email válido');
+        }
+    });
+
+    function ValidateEmail(email) {
+        const re = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        return re.test(String(email).toLowerCase());
+    }
+});
