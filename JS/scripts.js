f28f9cc0f40c490217f38e842d4e954602f93b21
JoseLr422 <raperoelement@gmail.com>
2024-06-27 21:44:55 -0500

    Update scripts.js

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,12 +1,5 @@
 // scripts.js
 
-// Añadir funcionalidad de clic al botón "Join NOW"
-document.querySelector('.join-now').addEventListener('click', function() {
-    alert('¡Gracias por unirte a MindWell!');
-});
-
-// Puedes añadir más lógica para otras interacciones en la página aquí
-
 document.addEventListener('DOMContentLoaded', () => {
     const form = document.querySelector('form[name="frm"]');
     form.addEventListener('submit', (event) => {
@@ -15,13 +8,16 @@ document.addEventListener('DOMContentLoaded', () => {
         const femail = form.elements['femail'].value;
         const fphone = form.elements['fphone'].value;
 
-
         if (!fname || !flastname || !femail || !fphone) {
             event.preventDefault();
             alert('Por favor, complete los datos del formulario');
         } else if (!ValidateEmail(femail)) {
             event.preventDefault();
             alert('Por favor, ingrese un email válido');
+        } else {
+            event.preventDefault(); // Evitar el envío del formulario real para mostrar el mensaje
+            alert('¡Gracias por unirte a MindWell!');
+            // Aquí puedes añadir más lógica, como redirigir o limpiar el formulario
         }
     });
 
